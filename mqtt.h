3705ed8d28498a819de10c9045743e5a18375fd6
maelponchant <mael.ponchant@outlook.com>
2020-04-25 20:06:11 +0200

    Added support for commands over mqtt

--- mqtt.h
+++ mqtt.h
@@ -6,7 +6,12 @@
 #define MQTT_PASSWORD ""
 #define MQTT_RECONNECT_TIMEOUT 5000
 
-#define DELETED_USERS_TOPIC "deleted_users"
+//Topics
+#define DOORLOCK_TOPIC "hosts/doorlock"
+
+//Commands
+#define CMD_REBOOT "reboot"
+#define CMD_DELETE_USER "delete_user"
 
 WiFiClient wifi_client;
 PubSubClient mqtt_client(wifi_client);
@@ -20,8 +25,20 @@ void on_message(char* topic, byte* message, unsigned int length)  {
     msg_str += (char)message[i];
   }
 
-  if(String(topic) == DELETED_USERS_TOPIC)  {
-    //delete_fingerprint(int(msg_str));
+  if(String(topic) == DOORLOCK_TOPIC)  {
+    StaticJsonBuffer<200> json_buffer;
+    JsonObject& json_msg = json_buffer.parseObject(msg_str);
+
+    if(json_msg.success() && json_msg.containsKey("command"))  {
+      String command;
+      json_msg["command"].printTo(command);
+      if(command == CMD_REBOOT) {
+        ESP.restart();
+      }
+      else if(command == CMD_DELETE_USER) {
+        
+      }
+    }
   }
 }
 
@@ -32,7 +49,7 @@ void init_mqtt()  {
   Serial.print("Connecting to MQTT broker..");
   if (mqtt_client.connect("IOT Doorlock")) {
     Serial.println("Connected");
-    mqtt_client.subscribe(DELETED_USERS_TOPIC);
+    mqtt_client.subscribe(DOORLOCK_TOPIC);
   } else {
     Serial.print("Failed, rc=");
     Serial.print(mqtt_client.state());
@@ -48,7 +65,7 @@ void reconnect_mqtt() {
     Serial.print("MQTT broker disconnected, reconnecting...");
     if (mqtt_client.connect("IOT Doorlock")) {
       Serial.println("Connected");
-      mqtt_client.subscribe(DELETED_USERS_TOPIC);
+      mqtt_client.subscribe(DOORLOCK_TOPIC);
     } else {
       Serial.print("Failed to connect, rc=");
       Serial.print(mqtt_client.state());
